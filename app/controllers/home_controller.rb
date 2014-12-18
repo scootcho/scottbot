015b8d6fbb574da0b9d2cfb5397718369f405c93
Scott Yu <scott.tj.yu@gmail.com>
2014-12-18 10:15:32 +0800

    update contact action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,10 @@ class HomeController < ActionController::Base
   def index
 
   end
+
+  def submit_request
+    ContactMailer.notify(params[:email], params[:name], params[:contact_number], params[:message])
+    redirect_to root_url, { :notice => "Thanks for contacting me! I'll be in touch shortly!" }
+  end
+
 end
